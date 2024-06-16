c54dda54d3ff45bba18f3b62c4a7bc64fb6bacaf
galo <marc_tony22@outlook.com>
2024-06-15 21:17:10 -0500

    se agregro al mostrar inventario se muestran sus productos

--- flows/intermediate-flow.js
+++ flows/intermediate-flow.js
@@ -1,22 +1,21 @@
 const { join } = require("path");
 const { readFileSync } = require("fs");
-const { addKeyword } = require("@bot-whatsapp/bot");
+const { addKeyword, addAnswer } = require("@bot-whatsapp/bot");
 const { flowReporteSelecionar } = require("./report-selection-flow");
 
-const flowintermedio = addKeyword("INTERMEDIATE_FLOW").addAnswer(
-  `¿Que te interesa?`,
-  //Espera a que el cliente responda
+const flowintermedio = addKeyword("INTERMEDIATE_FLOW")
+.addAnswer('¿Qué te interesa?')
+.addAnswer('Puede realizar la operación que desee escribiendo "solicito".',
+  // Espera a que el cliente responda
   { capture: true },
   async (ctx, { fallBack, gotoFlow }) => {
-    //se maneja la respuesta del usuario aqui
-    
-    if (ctx.body.toLowerCase() == "reporte") {
+    // Se maneja la respuesta del usuario aquí
+    if (ctx.body.toLowerCase() === "solicito") {
       return gotoFlow(flowReporteSelecionar);
+    } else {
+      return fallBack("Lo siento, no entendí su respuesta. Debe escribir 'solicito' para visualizar las opciones disponibles.");
     }
-    return fallBack("Lo siento, no entendí tu respuesta, debe escribir reporte");
   },
   [flowReporteSelecionar]
 );
-
-
 module.exports = {flowintermedio};
--- flows/report-selection-flow.js
+++ flows/report-selection-flow.js
@@ -1,19 +1,44 @@
 const { flujoFinal } = require("./final-flow");
 const { InventarioApi } = require("../api/inventario-api");
 const { addKeyword } = require("@bot-whatsapp/bot");
-const { flowdesicion1 } = require("./desicion1-flow");
+
 
 const flowReporteSelecionar = addKeyword("reporte_chat_bot")
+  .addAnswer('Si desea verificar un producto en particular escriba el nombre del producto')
+  .addAnswer('Tambien tiene la opción de un reporte detallado')
   .addAnswer([
-    "Que reporte te interesa?",
-    "a continuación te envio los tipos de reportes: (Marque 1 o 2)",
-    "1. Reporte producto \n2. Reporte stock",
+    "¿Que reporte te interesa?",
+    "estos son los tipos de reportes disponibles: (Marque 1 , 2 o 3)",
+    "1. Reporte producto \n2. Reporte stock \n3. Reporte de solo Productos con bajo stock",
   ])
   .addAction(
     { capture: true, idle: 10000 },
     async (ctx, { flowDynamic, gotoFlow }) => {
       if (ctx?.idleFallBack) {
         return gotoFlow(flujoFinal);
+      }
+
+      const userInput = ctx.body.trim();
+
+      if (isNaN(userInput)) {
+        try {
+          const productos = await InventarioApi.obtenerProducto(userInput);
+          if (productos && productos.comentario) {
+            await flowDynamic(`Su producto '${userInput}' tiene '${productos.comentario}'.`);
+            await flowDynamic(`el umbral minimo es '${productos.stockMinimo}' y tiene el total de '${productos.stocktotal}' cantidad de productos.`);
+            return gotoFlow(flujoFinal);
+          } else {
+            await flowDynamic("No se encontró el producto. Por favor, intenta de nuevo.");
+            await flowDynamic("ingrese el nombre del producto o las opciones");
+            await flowDynamic("1. Reporte producto");
+            await flowDynamic("2. Reporte stock");
+            await flowDynamic("3. Reporte de solo Productos con bajo stock");
+            return; 
+          }
+        } catch (error) {
+          await flowDynamic("Hubo un problema al buscar el producto. Por favor, intenta de nuevo ");
+          return; 
+        }
       }
 
       let outputFilePath;
@@ -26,8 +51,11 @@ const flowReporteSelecionar = addKeyword("reporte_chat_bot")
         case 2:
           outputFilePath = await InventarioApi.obtenerReporteStock();
           break; // Agregado para salir del switch después de ejecutar el case
+        case 3:
+          outputFilePath = await InventarioApi.obtenerProductoBajo();
+          break;
         default:
-          await flowDynamic("Opción inválida. Por favor marque 1 o 2.");
+          await flowDynamic("Opción inválida. Por favor marque 1 , 2 o 3.");
           return; // Agregado para salir de la función si la opción es inválida
       }
 
